421fc5b17992fffab1400ba5671a3f1282223d89
Pham Ngoc Hung <117938670+gherangme@users.noreply.github.com>
2024-10-06 00:27:05 +0700

    Update ApiResponse.java

--- src/main/java/com/hungpham/identityservice/dto/ApiResponse.java
+++ src/main/java/com/hungpham/identityservice/dto/ApiResponse.java
@@ -9,7 +9,6 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-// field nào null nó sẽ không kèm vào json -> giúp object nhỏ gọn hơn
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
     @Builder.Default
